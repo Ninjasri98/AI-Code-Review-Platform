121297e72d81effc8d035ea8d4b62e54c04510cf
Ninjasri98 <palitsriparno@gmail.com>
2026-01-16 11:24:42 +0530

    added logout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Logout } from "@/modules/auth/components/logout";
 import { requireAuth } from "@/modules/auth/utils/auth-utils";
 
 export default async function Home() {
   await requireAuth();
   return (
     <div className="flex flex-col items-center justify-center">
-
+      <Logout>
+        <Button>
+          Logout
+        </Button>
+      </Logout>
     </div>
   );
 }
