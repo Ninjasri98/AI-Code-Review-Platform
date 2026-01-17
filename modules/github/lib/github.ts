e50518b2e2e247c694d0fe749e068bacfff3274d
Ninjasri98 <palitsriparno@gmail.com>
2026-01-17 12:16:53 +0530

    added functions to get access token and fetch user contributions

---
+++ modules/github/lib/github.ts
@@ -0,0 +1,77 @@
+import { auth } from "@/lib/auth"
+import prisma from "@/lib/db"
+import { headers } from "next/headers"
+import { Octokit } from "octokit"
+
+interface ContributionData {
+    user: {
+        contributionsCollection: {
+            contributionCalendar: {
+                totalContributions: number;
+                weeks: {
+                    contributionDays: {
+                        contributionCount: number;
+                        date: string | Date;
+                        color: string;
+                    }[];
+                }[];
+            };
+        };
+    };
+}
+
+//method for getting github access token
+export const getGithubToken = async() =>{
+    const session = await auth.api.getSession({
+        headers : await headers()
+    })
+
+    if(!session){
+        throw new Error("Unauthorized")
+    }
+
+    const account = await prisma.account.findFirst({
+        where : {
+            userId : session.user.id,
+            providerId : "github"
+        }
+    })
+
+    if(!account?.accessToken){
+        throw new Error("GitHub access token not found")
+    }
+
+    return account.accessToken;
+}
+
+export async function fetchUserContributions(token: string, username: string) {
+    const octokit = new Octokit({
+        auth: token,
+    })
+    const query = `
+    query($username: String!) {
+        user(login: $username) {
+            contributionsCollection {
+                contributionCalendar {
+                    totalContributions
+                    weeks {
+                        contributionDays {
+                            contributionCount
+                            date
+                            color
+                        }
+                    }
+                }
+            }
+        }
+    }
+    `
+    try {
+        const response: ContributionData= await octokit.graphql(query,{
+            username
+        })
+        return response.user.contributionsCollection.contributionCalendar;
+    } catch (error) {
+        throw new Error("Failed to fetch user contributions : "+ error);
+    }
+}
